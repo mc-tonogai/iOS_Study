3e9a312c382d495410436d9959c44536c99200f5
殿谷将人 <>
2021-10-08 18:17:43 +0900

    算数クイズアプリをコミット

---
+++ study03/ToPassDataProtocol.swift
@@ -0,0 +1,8 @@
+//
+//  ToPassDataProtocol.swift
+//  study03
+//
+//  Created by 殿谷将人 on 2021/10/05.
+//
+
+import Foundation
---
+++ study03/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  study03
+//
+//  Created by 殿谷将人 on 2021/09/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
